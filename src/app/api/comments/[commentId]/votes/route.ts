732e54248beb28808e579efd56254604b4a23948
Aryan Sunil Patil <aryanpatil1503@gmail.com>
2025-05-04 16:18:06 -0400

    updated code -5

--- src/app/api/comments/[commentId]/votes/route.ts
+++ src/app/api/comments/[commentId]/votes/route.ts
@@ -1,6 +1,4 @@
 import { NextResponse } from 'next/server';
-import { NextResponse } from 'next/server';
-import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
 // POST /api/comments/[commentId]/votes
